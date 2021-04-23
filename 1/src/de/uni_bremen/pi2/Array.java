4faee0efd4e00d35689209fd71a1a75466cbfc13
takaiser-oss <takaiser@uni-bremen.de>
2021-04-23 17:12:14 +0200

    2

--- 1/src/de/uni_bremen/pi2/Array.java
+++ 1/src/de/uni_bremen/pi2/Array.java
@@ -1,6 +1,5 @@
 package de.uni_bremen.pi2;
 import java.util.Iterator;
-
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
--- 1/test/de/uni_bremen/pi2/ArrayTest.java
+++ 1/test/de/uni_bremen/pi2/ArrayTest.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ArrayTest
-{
+public class ArrayTest{
+public static void main(String[]args){
+    Array<String> test =new Array<>(10);
+    System.out.println(test.capacity());
+    System.out.println(test.size());
+    test.set(3,"abc");
+
+
+}
 }
