e9981bd78641f967a432fdcecd440b32624bbae0
Yali-Zhang-uniBremen <77195320+Yali-Zhang-uniBremen@users.noreply.github.com>
2021-04-25 10:56:45 +0200

    Ü1 updates

--- Übungsblatt 1 Kaiser, Zhang/test/de/uni_bremen/pi2/ArrayTest.java
+++ Übungsblatt 1 Kaiser, Zhang/test/de/uni_bremen/pi2/ArrayTest.java
@@ -4,13 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.Iterator;
+
 public class ArrayTest{
+
+    static void printArray(Array a){
+        Iterator it=a.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
 public static void main(String[]args){
-    Array<String> test =new Array<>(10);
-    System.out.println(test.capacity());
-    System.out.println(test.size());
-    test.set(3,"abc");
+    Array<Integer> test1=new Array<>(0);
+    System.out.println(test1.capacity());
+    System.out.println(test1.size());
 
+    Array<String> test2 =new Array<>(10);
+    System.out.println(test2.capacity());
+    System.out.println(test2.size());
 
+    test1.set(3,123);
+    test1.set(5,456);
+    test1.set(12,789);
+
+    test2.set(3,"abc");
+    test2.set(5,"ajf");
+    test2.set(12,"sue");
+    test2.set(32,"kjl");
+    test2.set(54,"ljd");
 }
 }
